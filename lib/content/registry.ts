c840471dc00d0d7c520a5539a3042987a41e3f5e
Allan Kiche <allankiche18@gmail.com>
2026-09-21 13:06:31 +0300

    Fix week 27 not showing: register in sync content registry

--- lib/content/registry.ts
+++ lib/content/registry.ts
@@ -29,6 +29,7 @@ import week23 from "./week-23";
 import week24 from "./week-24";
 import week25 from "./week-25";
 import week26 from "./week-26";
+import week27 from "./week-27";
 
 
 const CONTENT_REGISTRY: Record<number, DayContent[]> = {
@@ -57,7 +58,8 @@ const CONTENT_REGISTRY: Record<number, DayContent[]> = {
   23: week23,
   24: week24,
   25: week25,
-  26: week26, 
+  26: week26,
+  27: week27,
 };
 
 export function getWeekContent(weekId: number): DayContent[] | null {
